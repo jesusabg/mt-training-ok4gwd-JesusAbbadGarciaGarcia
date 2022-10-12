620f37a54c3f37b5bf6ecd31e581ba63ac588e07
jesusabg <87103495+jesusabg@users.noreply.github.com>
2022-10-12 15:42:12 -0500

    3 files modified, 1 added

--- src/app/mt-sample-exercise/mt-sample-list-index.component.ts
+++ src/app/mt-sample-exercise/mt-sample-list-index.component.ts
@@ -3,17 +3,16 @@ import { Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { DataService } from './data.service';
 import { Farm } from './farm';
+import { SelectedFarmService } from './selected-farm.service';
 
 @Component({
   selector: 'mt-sample-list',
   templateUrl: './mt-sample-list-index.component.html',
 })
 export class MtSampleListIndexComponent {
-  constructor(private _selected: DataService) {}
+  constructor(private _selected: SelectedFarmService) {}
 
-  ngOnInit() {
-    this.getFarms();
-  }
+  ngOnInit() {}
   selectedData: string;
   optionSelect: string;
   aa: boolean = false;
@@ -36,21 +35,23 @@ export class MtSampleListIndexComponent {
   selectedValue() {
     this.selectedData = this.optionSelect;
     console.log(this.optionSelect);
+    this.getFarms();
   }
 
-  getFarms() {
-    this._selected.getAllTasks().subscribe({
-      next: (response) => {
-        console.log(response);
-        this.farmsData = response;
-      },
-      error: (error) => {
-        console.log(error);
-        console.log(' error');
-      },
-    });
+  async getFarms() {
+    const response = await this._selected.getAllTasks();
+    this.farmsData = response;
   }
 
+  async getFarmsError(event) {
+    try {
+      const response = await this._selected.getAllTasksError(event);
+      response;
+    } catch (error) {
+      console.log('Error al traer la lista');
+      alert('Ocurrio una excepcion al traer la lista de granjas');
+    }
+  }
   setIndex(ii) {
     this.aa = ii;
     console.log;
---
+++ src/app/mt-sample-exercise/selected-farm.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class SelectedFarmService {
+  constructor(private http: HttpClient) {}
+  private _baseURI = 'assets/mock-data.json';
+  getAllTasks() {
+    const path = this._baseURI;
+    return this.http.get<[]>(path).toPromise();
+  }
+
+  getAllTasksError(event) {
+    const path = this._baseURI;
+    return this.http.get<[]>(event, path).toPromise();
+  }
+}
